199323aee6fc8c693edc55aaa05f53debc31fcf4
Fatih Rahman Mert <fatihmert3858@gmail.com>
2022-01-01 19:26:35 +0300

    create index file

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+
+app.get("/", (req:express.Request, res: express.Response) => {
+    
+    res.send("Tracker Api v1");
+    res.end();
+})
+
+
+app.listen(port, () => {
+
+    console.log("server is running...");
+    
+})
